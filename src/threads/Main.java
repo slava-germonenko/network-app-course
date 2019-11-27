d6c6d26224da20a919c5b95a7426ad13411c76a4
Viacheslav Germonenko <v.germonenko@itechart-group.com>
2019-11-27 19:24:48 +0300

    Remove unused images. Updated balloon image.

--- src/threads/Main.java
+++ src/threads/Main.java
@@ -5,35 +5,38 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main extends JFrame {
-    private static Image balloonImage;
+    private static BufferedImage balloonImage;
     private static Image explosionImage;
     private static JButton startStopButton;
     private static ArrayList<JLabel> balloonLabels;
     private static ArrayList<Integer> velocities;
 
-    private static int positionY = 350;
+    private static int startPosition = 780;
 
     private static boolean stopped = true;
 
     public Main() {
         setTitle("ИПР 2");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setSize(640, 400);
+        setSize(1080, 720);
         setLocationRelativeTo(null);
         setResizable(false);
 
         try {
-            balloonImage = ImageIO.read(new File("src/threads/balloon.png"));
-            explosionImage = ImageIO.read(new File("src/threads/explosion.png"));
+            BufferedImage bufferedBalloonImage = ImageIO.read(new File("src/threads/balloon.jpg"));
+            balloonImage = new BufferedImage(70, 70, bufferedBalloonImage.getType());
+            balloonImage.createGraphics().drawImage(bufferedBalloonImage, 6, 13,70, 70, null);
+
+            BufferedImage bufferedExplosionImage = ImageIO.read(new File("src/threads/explosion.png"));
         } catch (IOException ignored) {
             return;
         }
-
 
         balloonLabels = new ArrayList<>();
         velocities = new ArrayList<>();
@@ -41,7 +44,7 @@ public class Main extends JFrame {
         for (int i = 0; i <5 ; i++) {
             var label = new JLabel(new ImageIcon(balloonImage));
             label.setText(String.valueOf(i));
-            label.setBounds(i * 100 + 60, 350, 50, 50);
+            label.setBounds(i * 200 + 60, startPosition, 70, 70);
             int finalI = i;
             label.addMouseListener(new MouseAdapter() {
                 public int index = finalI;
@@ -79,6 +82,7 @@ public class Main extends JFrame {
             content.add(balloonLabels.get(i));
         }
 
+        this.setBackground(Color.WHITE);
         setContentPane(content);
     }
 
@@ -101,7 +105,7 @@ public class Main extends JFrame {
                     }
                 }
                 try {
-                    Thread.sleep(10);
+                    Thread.sleep(30);
                 } catch (InterruptedException ignored) {
                 }
             }
